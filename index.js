810ba1981da32cf6ca4028ecc2d4f5fcb51f1685
Alex <chaza.mckay@me.com>
2022-02-17 15:40:13 +1030

    base code

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const Employee = require("./lib/Employee");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const fs = require('fs');
 
 // Initialize a new employee Object
 const employee = new Employee();
 
 // Start by getting name
 employee.getName();
+console.log(employee.name);
 
 
